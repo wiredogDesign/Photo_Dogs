e6f4bf82ad92d83c6a77238677818c26612397de
Tom <wiredogDesign@icloud.com>
2022-12-11 09:45:13 +0000

    Changed header image (again) Changed how cat description is provided

--- _assets/custom.js
+++ _assets/custom.js
@@ -1,6 +1,7 @@
 var trigger = document.querySelector('header');
+var catechism = document.querySelector('.catechism').textContent;
 var info = document.querySelector('.info');
-var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1 + " dogs + 1 cat";
+var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1 + " dogs + " + catechism;
 document.querySelector('header').setAttribute('data-image-counter', imageCount);
 //
 function toggleClass() {
