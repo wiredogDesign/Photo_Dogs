13cb5460cdfd9297e81697fc5227cc7927993f98
Tom <wiredogDesign@icloud.com>
2023-08-28 13:08:18 +0100

    Removed unneccessary code

--- _assets/custom.js
+++ _assets/custom.js
@@ -1,9 +1,5 @@
 var header = document.querySelector('header');
-var catechism = document.querySelector('.catechism').textContent;
 var headerInfo = document.querySelector('.header-info');
-var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1 + " dogs + " + catechism;
-document.querySelector('header').setAttribute('data-image-counter', imageCount);
-//
 function toggleHeaderInfo() {
     headerInfo.classList.toggle('show-info');
 }
